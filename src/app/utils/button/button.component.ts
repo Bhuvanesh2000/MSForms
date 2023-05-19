8b9673ce70a13f0d0e5706e65f962006825c5337
Bhuvanesh Sutraye <sutrayebhuvaneshrao@gmail.com>
2023-05-19 20:46:24 +0530

    button component

---
+++ src/app/utils/button/button.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'ms-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+
+export class ButtonComponent {
+  @Input() btnLabel: string = 'Button';
+  @Input() btnType: string = 'button';
+  @Output() btnClick = new EventEmitter<any>();
+
+  emitEvent(e: any) {
+    this.btnClick.emit(e);
+  }
+}
